a3032da4da86cf6c4a20cdcb7218379bdcca2031
Ricky <Ricky@192.168.1.40>
2016-11-01 22:32:15 +0100

    Boletin 4

---
+++ src/boletin4_1/Boletin4_1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin4_1;
+
+/**
+ *
+ * @author Ricky
+ */
+public class Boletin4_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Coches aceler = new Coches();
+        aceler.acelerar(12);
+    }
+    
+}
---
+++ src/boletin4_1/Coches.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin4_1;
+
+/**
+ *
+ * @author Ricky
+ */
+public class Coches {
+        private int velocidade;
+        
+        public Coches(){
+            velocidade = 0;
+        }
+        public int getVelocidade(){
+            return velocidade;
+        }
+        public void acelerar(int valor){
+            valor += velocidade;         
+        }
+        public void frenar(int menos){
+            menos -= velocidade;            
+        }
+        
+}
